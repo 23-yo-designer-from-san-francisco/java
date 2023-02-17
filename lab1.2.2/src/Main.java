112fa33a8e54c5360e732638fe8c4ccd457b47ba
user <user@user.user>
2023-02-17 17:14:18 +0300

    Added lab 1.2.2

---
+++ lab1.2.2/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Enter n> ");
+        Scanner s = new Scanner(System.in);
+        int n = Integer.parseInt(s.next());
+        Integer[] nums = new Integer[n];
+
+        for (int i = 0; i < n; ++i) {
+            System.out.printf("Enter number %d>", i);
+            nums[i] = Integer.parseInt(s.next());
+        }
+
+        List<Integer> numbers = Arrays.asList(nums);
+
+        System.out.printf("Greatest: %s\n", String.join(", ", Collections.max(numbers).toString()));
+        System.out.printf("Smallest: %s\n", String.join(", ", Collections.min(numbers).toString()));
+    }
+}
